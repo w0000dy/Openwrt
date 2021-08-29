3eae9253ff026511d98a846fc86879b05603ff5b
w0000dy <42455594+w0000dy@users.noreply.github.com>
2021-08-29 20:48:56 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,5 +12,7 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
-git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
+
+ZZZ="package/lean/default-settings/files/zzz-default-settings"
+sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
 
