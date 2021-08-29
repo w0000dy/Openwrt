7a56ebb79eb05acc1d6a7441d545f2447a7eaf6d
w0000dy <42455594+w0000dy@users.noreply.github.com>
2021-08-29 12:56:36 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,4 +16,4 @@
 # Add a feed source
 echo 'src-git kenzo https://github.com/kenzok8/openwrt-packages' >>feeds.conf.default
 echo 'src-git small https://github.com/kenzok8/small' >>feeds.conf.default
-git clone https://github.com/esirplayground/luci-app-poweroff.git package/lean/luci-app-poweroff
+
