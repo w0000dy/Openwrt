f68202cec6cabd4bb4608f9ae04519c33f7e39ac
w0000dy <42455594+w0000dy@users.noreply.github.com>
2021-08-29 12:40:07 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -13,5 +13,4 @@
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.50.1/g' package/base-files/files/bin/config_generate
 
-ZZZ="package/lean/default-settings/files/zzz-default-settings"
-sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ
+
